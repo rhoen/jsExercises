43e6ed8404478d1e557495686a3816f8a78687f3
Felicia+Rhoen <Felicia+Rhoen@appacademy.io>
2015-04-23 12:34:43 -0400

    add myMap function to EnumBlks

--- EnumerableBlocks.js
+++ EnumerableBlocks.js
@@ -5,3 +5,29 @@ Array.prototype.myEach = function(func) {
 
   return this;
 };
+
+Array.prototype.myMap = function(func) {
+  var result = [];
+
+  var mapFunc = function (val){
+    result.push(func(val));
+  };
+
+  this.myEach(mapFunc);
+
+  return result;
+};
+
+var addOne = function(val) {
+  return val + 1;
+};
+
+class Array
+def my_map(&blk)
+  result = []
+  self.each do |el|
+    result << blk.call(el)
+  end
+
+  result
+end
