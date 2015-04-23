c23779a53d7493c77d26d4858940030287a0ef33
Felicia+Rhoen <Felicia+Rhoen@appacademy.io>
2015-04-23 11:30:11 -0400

    add twoSum monkey patch method to Array

--- js_exercises.js
+++ js_exercises.js
@@ -9,3 +9,22 @@ var uniq = function (arr) {
 
   return result;
 };
+
+
+Array.prototype.twoSum = function() {
+  var result = [];
+
+  for(i = 0; i < this.length; i++) {
+    for(j = i + 1; j < this.length; j++) {
+      if(this[i] + this[j] === 0) {
+        result.push([i, j]);
+      }
+    }
+  }
+
+  return result;
+};
+
+var myTranspose = function(arr) {
+
+};
